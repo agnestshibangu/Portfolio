2bca4c7dd7ded1af89515494321786b474f4d742
Agnes <agnes.tshibangugrillon@gmail.com>
2022-05-25 20:38:50 -0300

    description align right

--- src/Components/Front.js
+++ src/Components/Front.js
@@ -23,37 +23,62 @@ function Front() {
                     </div>
                 })} */}
 
-
-
             <div className='project-display-container'>
+
                 <div className='project-infos'>
-                    <h3 className='project-title'>project 1 : reservia</h3>
-                    <p className='project-description'>projet d'integration HTML et CSS à partir d'une maquette pour un site de reservation</p>
-                    <img className='project-image' src={reserviaImg} alt="" />
-                    <a href="https://reservia-project1-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
-                    
+
+                    <div className='projects-infos-left'>
+                        <a href="https://reservia-project1-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+                        <a href="https://reservia-project1-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+
+                    </div>
+
+                    <div className='projects-infos-right'>
+                        <h3 className='project-title'>project 1 : reservia</h3>
+                        <p className='project-description'>projet d'integration HTML et CSS à partir d'une maquette pour un site de reservation</p>
+                        <img className='project-image' src={reserviaImg} alt="" />
+                    </div>
+
+
                 </div>
+
+
+                <div className='project-infos'>
+
+                    <div className='projects-infos-left'>
+                        <a href="https://ohmyfood-project2-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+                        <a href="https://ohmyfood-project2-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+
+                    </div>
+
+                    <div className='projects-infos-right'>
+                        <h3 className='project-title'>ohmyfood</h3>
+                        <p className='project-description'>project d'integration à partir d'une maquette avec des animations CSS complexes et un site structuré en plusieurs pages</p>
+                        <img className='project-image' src={ohmyfoodImg} alt="" />
+                    </div>
+
+
+                </div>
+
+                <div className='project-infos'>
+
+                    <div className='projects-infos-left'>
+                        <a href="https://la-chouette-agence-seo-p4.netlify.app"><button >Full website here</button></a>
+                        <a href="https://la-chouette-agence-seo-p4.netlify.app"><button >Full website here</button></a>
+
+                    </div>
+
+                    <div className='projects-infos-right'>
+                        <h3 className='project-title'>la chouette agence</h3>
+                        <p className='project-description'>project d'integration HTML et CSS à partir d'une maquette pour un site de reservation</p>
+                        <img className='project-image' src={lachouetteagenceImg} alt="" />
+                    </div>
+
+
+                </div>
+
             </div>
 
-            <div className='project-display-container'>
-                <div className='project-infos'>
-                    <h3 className='project-title'>project 2 : ohmyfood</h3>
-                    <p className='project-description'>project d'integration à partir d'une maquette avec des animations CSS complexes et un site structuré en plusieurs pages</p>
-                    <img className='project-image' src={ohmyfoodImg} alt="" />
-                    <a href="https://ohmyfood-project2-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
-                
-                </div>
-            </div>
-
-            <div className='project-display-container'>
-                <div className='project-infos'>
-                    <h3 className='project-title'>project 3 : la chouette agence</h3>
-                    <p className='project-description'>project d'integration HTML et CSS à partir d'une maquette pour un site de reservation</p>
-                    <img className='project-image' src={lachouetteagenceImg} alt="" />
-                    <a href="https://la-chouette-agence-seo-p4.netlify.app/"><button >Full website here</button></a>
-                   
-                </div>
-            </div>
 
 
 
--- src/Components/Reactproject.js
+++ src/Components/Reactproject.js
@@ -8,42 +8,65 @@ import primeurbioImg from './images/projectsscreen/capture-primeur-bio.PNG'
 function Reactproject() {
     return (
         <div className='section'>
-            <h2 className='front-end-projects-title'>react project</h2>
+
+            <h2 className='front-end-projects-title'>projets react</h2>
 
 
             <div className='project-display-container'>
+
                 <div className='project-infos'>
-                    <h3 className='project-title'>e-commerce</h3>
-                    <p className='project-description'>un réseau social en REACT</p>
-                    <img className='project-image' src={ecommerceImg} alt="" /> 
-                    <a href="https://art-shop-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+
+                    <div className='projects-infos-left'>
+                        <a href="https://art-shop-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+                        <a href="https://art-shop-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+
+                    </div>
+
+                    <div className='projects-infos-right'>
+                        <h3 className='project-title'>e-commerce</h3>
+                        <p className='project-description'>un site de e-commerce en REACT</p>
+                        <img className='project-image' src={ecommerceImg} alt="" />
+                    </div>
+
 
                 </div>
-            </div>
 
 
-            <div className='project-display-container'>
                 <div className='project-infos'>
-                    <h3 className='project-title'>groupomania</h3>
-                    <p className='project-description'>un réseau social en REACT</p>
-                    <img className='project-image' src={groupomaniaImg} alt="" /> 
-                    <a href="https://groupomania-social-network-p7.netlify.app/login"><button >Full website here</button></a>
+
+                    <div className='projects-infos-left'>
+                        <a href="https://groupomania-social-network-p7.netlify.app/login"><button >Full website here</button></a>
+                        <a href="https://groupomania-social-network-p7.netlify.app/login"><button >Full website here</button></a>
+
+                    </div>
+
+                    <div className='projects-infos-right'>
+                        <h3 className='project-title'>groupomania</h3>
+                        <p className='project-description'>un réseau social en REACT</p>
+                        <img className='project-image' src={groupomaniaImg} alt="" />
+                    </div>
+
 
                 </div>
-            </div>
 
-
-            <div className='project-display-container'>
                 <div className='project-infos'>
-                    <h3 className='project-title'>primeur bio</h3>
-                    <p className='project-description'>un site vitrine pour une entreprise de livraison de fruits et légumes bio</p>
-                    <img className='project-image' src={primeurbioImg} alt="" /> 
-                    <a href="https://primeur-bio-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+
+                    <div className='projects-infos-left'>
+                        <a href="https://primeur-bio-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+                        <a href="https://primeur-bio-agnes-tshibangu.netlify.app/"><button >Full website here</button></a>
+
+                    </div>
+
+                    <div className='projects-infos-right'>
+                        <h3 className='project-title'>primeur bie</h3>
+                        <p className='project-description'>site vitrine pour un primeur bio</p>
+                        <img className='project-image' src={primeurbioImg} alt="" />
+                    </div>
+
 
                 </div>
+
             </div>
-
-
 
         </div>
     )
