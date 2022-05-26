eec3431fd56c37058c002fb0279d86e59750d78a
Agnes <agnes.tshibangugrillon@gmail.com>
2022-05-25 23:52:46 -0300

    hobbies

--- src/Components/Artisticprojects.js
+++ src/Components/Artisticprojects.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import Blender from './images/projectsscreen/capture-blender.PNG'
+import Vector from './images/projectsscreen/phone.png'
+import '../style/sections.scss'
+import '../style/hobbies.scss'
+
 
 function Artisticprojects() {
   return (
-    <div>Artisticprojects</div>
+    <div className='section'>
+    <h2 className='front-end-projects-title'>hobbies</h2>
+
+
+    <div className='section-hobbies'>
+
+
+        <div className='blender-section'>
+            <img className='image-hobby' src={Blender} alt=""/>
+            <p className='description-hobby project-description'>3D, Blender</p>
+        </div>
+
+        
+        <div className='vector-section'>
+            <img className='image-hobby' src={Vector} alt=""/>
+            <p className='description-hobby project-description'>dessin vectoriel, A. Illustrator</p>
+        </div>
+
+
+    </div>
+
+
+
+
+
+    </div>
   )
 }
 
