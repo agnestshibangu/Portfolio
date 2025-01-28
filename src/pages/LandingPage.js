c332a70c9f9e381ca5ec1033f0dfafc3e389f8c1
agnestshibangu <agnes.tshibangugrillon@gmail.com>
2025-01-28 16:22:49 +0100

    progress bar and round

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -113,7 +113,7 @@ export default function LandingPage() {
           <div className='vertical-line-round reveal round2' />
           <div className='vertical-line-round reveal round3' />
           <div className='vertical-line-round reveal round4' />
-        
+          <div className='vertical-line-round reveal round5' />        
 
         </div>
 
