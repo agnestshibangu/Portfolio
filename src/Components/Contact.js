56157c049bfc0474afb9f82178fa2d7c3483d4bd
agnestshibangu <agnes.tshibangugrillon@gmail.com>
2025-01-28 16:09:58 +0100

    corrected link

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,7 +11,7 @@ function Contact() {
         <div className='contact-box'><p className="small-text-contact">feel free to reach out - I would love to welcome the opportunity to leverage my back-end expertise to contribute to your team !  🚀 </p></div>
         <div className='contact-box'>
           <p className="telephone">
-            <a href="www.linkedin..com/in/agnes-tshibangu-grillon">My linkedin profile</a>
+            <a href="https://www.linkedin.com/in/agnes-tshibangu-grillon">My linkedin profile</a>
           </p>
         </div>
         <div className='contact-box'><p className="email">agtshiba@student.42perpignan.fr</p></div>
