e4ba56c882828ebbaba65992e20599466ed72786
agnestshibangu <agnes.tshibangugrillon@gmail.com>
2025-01-30 14:06:11 +0100

    commit

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -74,7 +74,9 @@ export default function LandingPage() {
       
       <div className='top-page'>
 
+      <div className='parent-glowing'>
         <div className="glowing"></div> 
+      </div>
 
         <div className="name-presentation-box">
           <p className="hello_description">Hello ! I am</p>
